11f20bc13d4d94506d6532540869cf2f601ee891
lastagous <from.iphone5s@gmail.com>
2024-12-29 14:39:21 +0900

    [ADD] PIP 用のコンポーネントを追加

---
+++ src/app/component/cheatsheet/cheatsheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cheatsheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './cheatsheet.component.html',
+  styleUrl: './cheatsheet.component.less'
+})
+export class CheatsheetComponent {
+
+}
